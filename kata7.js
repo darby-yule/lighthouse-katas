6d60a4d11c34c5dc48a0e1bc6bf1e8e08b099144
Darby Yule <darby@theyulebrothers.com>
2022-05-07 16:07:35 -0700

    kata7.js

---
+++ kata7.js
@@ -0,0 +1,27 @@
+const checkAir = function (samples, threshold) {
+  let poll = 0;
+  for(let i = 0; i < samples.length; i++){
+    if(samples[i] === 'dirty'){
+      poll++;
+    }
+  }if(threshold <= poll / 10){
+    return 'Polluted';
+  }else{
+    return 'Clean';
+  }
+};
+
+console.log(checkAir(
+  ['clean', 'clean', 'dirty', 'clean', 'dirty', 'clean', 'clean', 'dirty', 'clean', 'dirty'],
+  0.3
+));
+
+console.log(checkAir(
+  ['dirty', 'dirty', 'dirty', 'dirty', 'clean'],
+  0.25
+));
+
+console.log(checkAir(
+  ['clean', 'dirty', 'clean', 'dirty', 'clean', 'dirty', 'clean'],
+  0.9
+))
